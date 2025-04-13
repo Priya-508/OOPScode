d4bdf2488473daf62611d06b5a10c5620beded5c
Priya-508 <priyakeshwani5@gmail.com>
2025-04-13 19:48:57 +0530

    Time: 5 ms (71.81%) | Memory: 44.3 MB (36.47%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
+        }
+//priya
+        int reversed = 0;
+        int original = x;
+
+        while (x > 0) {
+            int digit = x % 10;
+            reversed = reversed * 10 + digit;
+            x /= 10;
+        }
+
+        return original == reversed;
+    }
+}
